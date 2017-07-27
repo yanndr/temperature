2b55295c6a99f131709b8deff369f173c577e270
Yann <yann.druffin@gmail.com>
2017-07-27 13:52:21 -0700

    Fixed test Rankine display

--- temperature_test.go
+++ temperature_test.go
@@ -6,14 +6,14 @@ import (
 	"github.com/yanndr/temperature"
 )
 
-type Stringer interface {
+type stringer interface {
 	String() string
 }
 
 func TestString(t *testing.T) {
 	tt := []struct {
 		name        string
-		temperature Stringer
+		temperature stringer
 		result      string
 	}{
 		{name: "Print Celsius", temperature: temperature.NewCelsius(10), result: "10 °C"},
@@ -21,7 +21,7 @@ func TestString(t *testing.T) {
 		{name: "Print Delisle", temperature: temperature.NewDelisle(10), result: "10 °D"},
 		{name: "Print Kelvin", temperature: temperature.NewKelvin(10), result: "10 K"},
 		{name: "Print Reaumur", temperature: temperature.NewReaumur(10), result: "10 °Re"},
-		{name: "Print Rankine", temperature: temperature.NewRankine(10), result: "10°Ra"},
+		{name: "Print Rankine", temperature: temperature.NewRankine(10), result: "10 °Ra"},
 	}
 
 	for _, tc := range tt {
