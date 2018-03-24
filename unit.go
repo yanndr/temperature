f8de17f893eceacac654e6d73c18746c0c90d5f3
Yann Druffin <yann.druffin@gmail.com>
2018-03-24 11:51:09 -0700

    Had to expose units, not really happy about that

--- unit.go
+++ unit.go
@@ -2,13 +2,13 @@ package temperature
 
 type (
 	//Unit is a temperature unit type.
-	Unit       string
-	celsius    Unit
-	kelvin     Unit
-	fahrenheit Unit
-	rankine    Unit
-	delisle    Unit
-	reaumur    Unit
+	Unit           string
+	CelsiusUnit    Unit
+	KelvinUnit     Unit
+	FahrenheitUnit Unit
+	RankineUnit    Unit
+	DelisleUnit    Unit
+	ReaumurUnit    Unit
 
 	//Convertible provides function to convert from and to Kelvin unit.
 	Convertible interface {
@@ -19,63 +19,63 @@ type (
 
 const (
 	//Celsius temperature unit
-	Celsius = celsius("°C")
+	Celsius = CelsiusUnit("°C")
 	//Kelvin temperature unit
-	Kelvin = kelvin("K")
+	Kelvin = KelvinUnit("K")
 	//Fahrenheit temperature unit
-	Fahrenheit = fahrenheit("°F")
+	Fahrenheit = FahrenheitUnit("°F")
 	//Rankine temperature unit
-	Rankine = rankine("°Ra")
+	Rankine = RankineUnit("°Ra")
 	//Delisle temperature unit
-	Delisle = delisle("°D")
+	Delisle = DelisleUnit("°D")
 	//Reaumur temperature unit
-	Reaumur = reaumur("°Re")
+	Reaumur = ReaumurUnit("°Re")
 )
 
-func (celsius) ToKelvin(v float64) float64 {
+func (CelsiusUnit) ToKelvin(v float64) float64 {
 	return v + 273.15
 }
 
-func (celsius) FromKelvin(v float64) Temperature {
+func (CelsiusUnit) FromKelvin(v float64) Temperature {
 	return &temperature{v: v - 273.15, unit: Celsius}
 }
 
-func (kelvin) ToKelvin(v float64) float64 {
+func (KelvinUnit) ToKelvin(v float64) float64 {
 	return v
 }
 
-func (kelvin) FromKelvin(v float64) Temperature {
+func (KelvinUnit) FromKelvin(v float64) Temperature {
 	return &temperature{v: v, unit: Kelvin}
 }
 
-func (fahrenheit) ToKelvin(v float64) float64 {
+func (FahrenheitUnit) ToKelvin(v float64) float64 {
 	return (v + 459.67) * 5 / 9
 }
 
-func (fahrenheit) FromKelvin(v float64) Temperature {
+func (FahrenheitUnit) FromKelvin(v float64) Temperature {
 	return &temperature{v: v*9/5 - 459.67, unit: Fahrenheit}
 }
 
-func (rankine) ToKelvin(v float64) float64 {
+func (RankineUnit) ToKelvin(v float64) float64 {
 	return v * 5 / 9
 }
 
-func (rankine) FromKelvin(v float64) Temperature {
+func (RankineUnit) FromKelvin(v float64) Temperature {
 	return &temperature{v: v * 9 / 5, unit: Rankine}
 }
 
-func (delisle) ToKelvin(v float64) float64 {
+func (DelisleUnit) ToKelvin(v float64) float64 {
 	return 373.15 - v*2/3
 }
 
-func (delisle) FromKelvin(v float64) Temperature {
+func (DelisleUnit) FromKelvin(v float64) Temperature {
 	return &temperature{v: (373.15 - v) * 3 / 2, unit: Delisle}
 }
 
-func (reaumur) ToKelvin(v float64) float64 {
+func (ReaumurUnit) ToKelvin(v float64) float64 {
 	return v*5/4 + 273.15
 }
 
-func (reaumur) FromKelvin(v float64) Temperature {
+func (ReaumurUnit) FromKelvin(v float64) Temperature {
 	return &temperature{v: (v - 273.15) * 4 / 5, unit: Reaumur}
 }
